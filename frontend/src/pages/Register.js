a8110051a5b3be70e174e21731579d76b927bbb0
abhiyadav-2307 <abhi1611919@gmail.com>
2025-03-16 12:29:43 +0530

    Update backend URL in Login.js and Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/register', { name, email, password });
+      await axios.post('https://school-management-system-xs22.onrender.com/api/auth/register', { name, email, password });
       navigate('/login');
     } catch (err) {
       console.log(err);
